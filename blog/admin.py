fcf5439594f0faab809056a7691075a37c42afbe
Saniya Chikane <saniyachikane2709@gmail.com>
2025-09-30 00:35:15 +0530

    Fixed admin.py syntax error setting.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
-
 from django.contrib import admin
 from  .models import Post
 
 admin.site.register(Post)
 
-=======
 from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
->>>>>>> 25d3f77 (Deployment files setup and project initialization)
+
