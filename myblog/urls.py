7cc05df17acec2b1a35b210ef36ad40b7cd295ce
Saniya Chikane <saniyachikane2709@gmail.com>
2025-10-01 16:14:38 +0530

    CRITICAL FIX: Cleaned up and corrected myblog/urls.py for login functionality

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,20 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('', include('django.contrib.auth.urls')), 
-   path('', include('blog.urls')),
-   path('blog/', include('blog.urls')),
-   path('accounts/', include('django.contrib.auth.urls')),
-]
+    path('admin/', admin.site.urls), 
+   
+    path('', include('django.contrib.auth.urls')), 
     
-
+    path('', include('blog.urls')),
+   
