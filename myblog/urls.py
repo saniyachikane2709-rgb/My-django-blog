a2bdd1e6d2423884f36103c02b4bd4f5cef099e8
Saniya Chikane <saniyachikane2709@gmail.com>
2025-10-01 14:05:18 +0530

    Final fix: Configured root path in myblog/urls.py to resolve Not Found error

--- myblog/urls.py
+++ myblog/urls.py
@@ -10,6 +10,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
    path('admin/', admin.site.urls),
+   path('', include('blog.urls')),
    path('blog/', include('blog.urls')),
    path('accounts/', include('django.contrib.auth.urls')),
 ]
